388a9a392f639dac70d47ab9413c39446b70d01a
t-trinker <github.5.tower@spamgourmet.com>
2025-06-27 13:56:47 +0200

    bookreport erstellt

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
 from stats import get_word_count
+from stats import get_book_text
+from stats import get_letter_count
+from stats import get_sorted_letter_dict
+
+def create_book_report(filepath: str) -> None:
+    booktext = get_book_text(filepath)
+    num_words = get_word_count(booktext)
+    num_letters = get_letter_count(booktext)
+    sorted_letters = get_sorted_letter_dict(num_letters)
+
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {filepath}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for letter in sorted_letters:
+        if letter["char"].isalpha():
+            print(f"{letter["char"]}: {letter["num"]}")
 
 def main() -> None:
-    num_words = get_word_count("books/frankenstein.txt")
-    print(f"{num_words} words found in the document")
-
+    create_book_report("books/frankenstein.txt")
+    
 main()
--- stats.py
+++ stats.py
@@ -1,5 +1,6 @@
-def get_word_count(filepath: str) -> int:
-    booktext = get_book_text(filepath)
+from typing import TypedDict
+
+def get_word_count(booktext: str) -> int:
     words = booktext.split()
     return len(words)
 
@@ -8,3 +9,38 @@ def get_book_text(filepath: str) -> str:
     with open(filepath) as f:
         file_contents = f.read()
         return file_contents
+
+
+def get_letter_count(booktext: str) -> dict[str, int]:
+    letters: dict[str, int] = {}
+
+    for letter in booktext:
+        letter = letter.lower()
+        if letter not in letters:
+            letters[letter] = 0
+        letters[letter] += 1
+
+    return letters
+
+
+class Letter_Dict(TypedDict):
+    char: str
+    num: int
+
+
+def sort_on(items: Letter_Dict) -> int:
+    return items["num"]
+
+
+def get_sorted_letter_dict(letters: dict[str, int]) -> list[Letter_Dict]:
+
+    sorted_letters: list[Letter_Dict] = []
+    for letter in letters:
+        letter_dict: Letter_Dict = {
+            "char": letter,
+            "num": letters[letter]
+        }
+        sorted_letters.append(letter_dict)
+    
+    sorted_letters.sort(reverse=True, key=sort_on)
+    return sorted_letters
